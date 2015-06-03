73b4e1129270d9b6c1d0f3a4d3a3b3b2d560bd7d
bapplejax <jake.bapple@gmail.com>
2015-06-02 22:41:03 -0500

    Added Aaron's Name
    to contact form

--- email.php
+++ email.php
@@ -1,6 +1,6 @@
 <?php 
 if(isset($_POST['submit'])){
-    $to = "info@satelliteprintlab.com"; // this is your Email address
+    $to = "info@satelliteprintlab.com,aaron@wsioutdoor.com"; // this is your Email address
     $from = $_POST['email']; // this is the sender's Email address
     $name = $_POST['name']; // this is the sender's Name
     $phone = $_POST['phone']; // this is the sender's Phone Number
